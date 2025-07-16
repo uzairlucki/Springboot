1663b05ae23991b04a2f50ca56756f097ae56977
Uzair Ahmad <32535552+uzairlucki@users.noreply.github.com>
2025-07-16 18:38:47 +0500

    3rd commit

--- src/main/java/com/dps/crud/model/Employee.java
+++ src/main/java/com/dps/crud/model/Employee.java
@@ -29,10 +29,67 @@ public class Employee {
 	private String position; // e.g., "Software Engineer", "HR Manager"
 	private double salary;
 	@CreatedDate
-	@Column(nullable = false, updatable = false)
+	@Column(nullable = false, updatable = false, name ="created_date" )
+
 	private LocalDateTime createdDate;
 
 	@LastModifiedDate
 	@Column(nullable = false)
 	private LocalDateTime modifiedDate;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(LocalDateTime modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
 }
